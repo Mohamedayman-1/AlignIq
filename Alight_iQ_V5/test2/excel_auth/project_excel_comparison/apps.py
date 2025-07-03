6d7e1a40eca5436bff961c8928c1d72d9f55cc43
Mohamedayman-1 <mayman@lightidea.org>
2025-07-03 22:21:15 +0400

    .delete

--- Alight_iQ_V5/test2/excel_auth/project_excel_comparison/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjectExcelComparisonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'project_excel_comparison'
--- Alight_iQ_V5/test2/excel_auth/project_excel_comparison/views/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth_views import *
-from .file_views import *
-from .comparison_views import *
-from .database_views import *
-from .admin_views import *
-from .utility_views import *
